94ce8fe7d144c4f3582fd087d03669daf2187681
fernandobouchet <fernandobouchet@gmail.com>
2021-12-27 17:57:11 -0300

    Added score table and other improvements

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const options = ["ROCK", "PAPER", "SCISSORS"];
+let userScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
   return options[Math.floor(Math.random() * options.length)];
@@ -6,28 +8,53 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection = computerPlay()) {
   let result = "";
-  if (playerSelection === computerSelection) {
-    result = "tie";
-  } else if (
-    (playerSelection === "ROCK" && computerSelection === "PAPER") ||
-    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
-    (playerSelection === "SCISSORS" && computerSelection === "PAPER")
-  ) {
-    result = "win";
-  } else {
-    result = "lose";
+  while (userScore < 5 && computerScore < 5) {
+    if (playerSelection === computerSelection) {
+      return `Its a tie! computer choice was ${computerSelection} too!`;
+    } else if (
+      (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+      (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+      (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+    ) {
+      userScore += 1;
+      result = "win";
+    } else {
+      computerScore += 1;
+      result = "lose";
+    }
+    return `You ${result}! computer choice was ${computerSelection}!`;
   }
+  return checkResult();
+}
 
-  return result;
+function checkResult() {
+  winner = "";
+  if (userScore === 5) {
+    winner = "You";
+  } else if (computerScore === 5) {
+    winner = "PC";
+  }
+  return `Final Score! ${winner} win!`;
 }
 const result = document.querySelector("#results");
 const resultContent = document.createElement("p");
 resultContent.className = "result-content";
 result.appendChild(resultContent);
 
+const userScoreView = document.querySelector("#userScore");
+const userScoreContent = document.createElement("p");
+userScoreView.appendChild(userScoreContent);
+
+const pcScoreView = document.querySelector("#pcScore");
+const pcScoreContent = document.createElement("p");
+pcScoreView.appendChild(pcScoreContent);
+
 const btns = document.querySelectorAll("input");
 btns.forEach((button) => {
   button.addEventListener("click", () => {
     resultContent.textContent = playRound(button.id);
+    userScoreContent.textContent = userScore;
+    pcScoreContent.textContent = computerScore;
+    checkResult();
   });
 });
