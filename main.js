4cabe66b20e17d331dd666a3a4763de878fff66b
fernandobouchet <fernandobouchet@gmail.com>
2021-12-27 16:49:43 -0300

    Add images, css style and optimized logic

--- main.js
+++ main.js
@@ -4,43 +4,28 @@ function computerPlay() {
   return options[Math.floor(Math.random() * options.length)];
 }
 
-function playRound(playerSelection) {
-  let computerSelection = computerPlay();
+function playRound(playerSelection, computerSelection = computerPlay()) {
   let result = "";
-  if (playerSelection === "ROCK") {
-    if (computerSelection === "SCISSORS") {
-      result = "You win! ROCK beats SCISSORS";
-    } else if (computerSelection === "PAPER") {
-      result = "You Lose! PAPER beats ROCK";
-    } else {
-      result = "Its a tie";
-    }
-  } else if (playerSelection === "PAPER") {
-    if (computerSelection === "ROCK") {
-      result = "You win! PAPER beats ROCK";
-    } else if (computerSelection === "SCISSORS") {
-      result = "You Lose! SCISSORS beats PAPER";
-    } else {
-      result = "Its a tie";
-    }
+  if (playerSelection === computerSelection) {
+    result = "tie";
+  } else if (
+    (playerSelection === "ROCK" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+  ) {
+    result = "win";
   } else {
-    if (computerSelection === "PAPER") {
-      result = "You win! SCISSORS beats PAPER";
-    } else if (computerSelection === "ROCK") {
-      result = "You Lose! ROCK beats SCISSORS";
-    } else {
-      result = "Its a tie";
-    }
+    result = "lose";
   }
+
   return result;
 }
 const result = document.querySelector("#results");
 const resultContent = document.createElement("p");
 resultContent.className = "result-content";
-
 result.appendChild(resultContent);
 
-const btns = document.querySelectorAll("button");
+const btns = document.querySelectorAll("input");
 btns.forEach((button) => {
   button.addEventListener("click", () => {
     resultContent.textContent = playRound(button.id);
